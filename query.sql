ef801abf6e633f1512527e7ec1677215772ad5bd
Yusuf Abdullah <yusufabdullah20000@gmail.com>
2026-06-22 12:22:26 +0600

    query 2 added

--- query.sql
+++ query.sql
@@ -75,3 +75,8 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 --Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 select match_id,fixture,base_ticket_price from matches where tournament_category='Champions League' and match_status='Available'
+
+
+
+--Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+select * from users where full_name Ilike 'Tanvir%' or full_name Ilike '%Haque%'
